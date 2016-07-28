59b206edd31dedf1f6d7cfcae8aa4feb45066368
Micaiah Parker <me@micaiahparker.com>
2016-07-28 00:09:52 -0400

    added token support

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import flask
+import os
+
+token = os.environ['TELEGRAM_API_TOKEN']
+url = 'https://api.telegram.org/bot{token}'.format(token=token)
+
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/{token}'.format(token=token))
+def token_route():
+    pass
 
 if __name__ == "__main__":
     app.run()
